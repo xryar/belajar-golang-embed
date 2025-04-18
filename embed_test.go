9611db514efa57e323a4a93a8eb409a9275a377f
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-04-18 18:26:19 +0700

    embed image

--- embed_test.go
+++ embed_test.go
@@ -3,6 +3,8 @@ package belajargolangembed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var version string
 
 func TestEmbedString(t *testing.T) {
 	fmt.Println(version)
+}
+
+//go:embed kucing_tengok_lah.jpg
+var image []byte
+
+func TestEmbedImage(t *testing.T) {
+	err := os.WriteFile("kucing_new.jpg", image, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
